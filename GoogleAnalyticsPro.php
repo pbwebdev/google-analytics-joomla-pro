cd7809bb6c34a4a99fd7eb4987bf0a764581150e
kermage <genealyson.torcende@gmail.com>
2020-10-20 18:17:45 +0800

    optimized methods

--- GoogleAnalyticsPro.php
+++ GoogleAnalyticsPro.php
@@ -26,32 +26,51 @@ class plgSystemGoogleAnalyticsPro extends JPlugin {
 	private $document = null;
 	protected $app;
 
+	private $user_id = 0;
+	private $config = array();
 
-	function plgGoogleAnalyticsPro( $subject, $params)
+
+	function plgGoogleAnalyticsPro($subject, $params)
 	{
 		parent::__construct($subject, $params);
+	}
+
+	function onAfterInitialise()
+	{
+		$this->document = JFactory::getDocument();
+		$this->user_id = JFactory::getUser()->id;
+
+		$this->config['verify'] = $this->params->get('verify');
+		$this->config['trackingID'] = $this->params->get('trackingID');
+		$this->config['trackingID2'] = $this->params->get('trackingID2');
+		$this->config['containerID'] = $this->params->get('containerID');
+		$this->config['userIDTracking'] = $this->params->get('userIDTracking');
+		$this->config['forceSSL'] = $this->params->get('forceSSL');
+		$this->config['pageviewTracking'] = $this->params->get('pageviewTracking');
+		$this->config['siteCurrency'] = $this->params->get('siteCurrency');
+		$this->config['enhancedLinkAttribution'] = $this->params->get('enhancedLinkAttribution');
+		$this->config['userPerformanceTiming'] = $this->params->get('userPerformanceTiming');
+		$this->config['ipAnonymize'] = $this->params->get('ipAnonymize');
 	}
 
 	//Generate all the output
 	function onBeforeCompileHead()
 	{
-		$this->document = JFactory::getDocument();
-
 		if ($this->app->isClient('administrator') || $this->document->getType() != 'html') {
 			return;
 		}
 
 		$output = '';
 
-		if ($this->params->get('verify')){
+		if ($this->config['verify']){
 			$output .= $this->webmasterVerify();
 		}
 
-		if ($this->params->get('trackingID') || $this->params->get('trackingID2')) {
+		if ($this->config['trackingID'] || $this->config['trackingID2']) {
 			$output .= $this->googleAnalyticsTag();
 		}
 
-		if ($this->params->get('containerID')) {
+		if ($this->config['containerID']) {
 			$output .= $this->googleTagManager();
 		}
 
@@ -64,118 +83,97 @@ class plgSystemGoogleAnalyticsPro extends JPlugin {
 			return;
 		}
 
-		if ($this->params->get('containerID')) {
+		if ($this->config['containerID']) {
 			$buffer = JFactory::getApplication()->getBody();
-			$noJSOutput = '<noscript><iframe src="https://www.googletagmanager.com/ns.html?id='.$this->params->get('containerID').'" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>';
-			$buffer= str_ireplace('</body>',$noJSOutput.'</body>',$buffer);
+			$script = '	<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=' . $this->config['containerID'] . '" height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+';
+			$buffer = str_ireplace('</body>', $script . '</body>', $buffer);
+
 			JFactory::getApplication()->setBody($buffer);
 		}
 	}
 
 	private function webmasterVerify()
 	{
-		$verify = $this->params->get('verify');
-
-		return '<meta name="google-site-verification" content="'.$verify.'" />';
-	}
-
-	private function userIDTracking()
-	{
-		$user = JFactory::getUser();
-		
-		return $user->id;
+		return '<meta name="google-site-verification" content="' . $this->config['verify'] . '" />
+';
 	}
 
 	private function userPerformanceTiming()
 	{
-		return 'if (window.performance) {
-			var timeSincePageLoad = Math.round(performance.now());
-			gtag(\'event\', \'timing_complete\', {
-				\'name\': \'load\',
-				\'value\': timeSincePageLoad,
-				\'event_category\': \'JS Dependencies\'
-			});
-		}
-';
+		return '	if (window.performance) {
+		var timeSincePageLoad = Math.round(performance.now());
+		gtag(\'event\', \'timing_complete\', {
+			\'name\': \'load\',
+			\'value\': timeSincePageLoad,
+			\'event_category\': \'JS Dependencies\'
+		});
 	}
-
-	private function isLoggedIn()
-	{
-		return JFactory::getUser()->id ? true : false;
+';
 	}
 
 	private function googleAnalyticsTag()
 	{
-		$trackingID = $this->params->get('trackingID');
-		$trackingID2 = $this->params->get('trackingID2');
-		$userIDTracking = $this->params->get('userIDTracking');
-		$forceSSL = $this->params->get('forceSSL');
-		$pageviewTracking = $this->params->get('pageviewTracking');
-		$siteCurrency = $this->params->get('siteCurrency');
-		$enhancedLinkAttribution = $this->params->get('enhancedLinkAttribution');
-		$userPerformanceTiming = $this->params->get('userPerformanceTiming');
-		$ipAnonymize = $this->params->get('ipAnonymize');
-
 		$buffer = '
-		<script async src="https://www.googletagmanager.com/gtag/js?id='.$trackingID.'"></script>
-		<script>
-		window.dataLayer = window.dataLayer || [];
-		function gtag(){dataLayer.push(arguments);}
-		gtag(\'js\', new Date());
-		';
+	<script async src="https://www.googletagmanager.com/gtag/js?id=' . $this->config['trackingID'] . '"></script>
+
+	<script>
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag(\'js\', new Date());
+';
 
 		$config = array();
 
-		if ($userIDTracking && $this->isLoggedIn()){
-			$config['user_id'] = $this->userIDTracking();
+		if ($this->config['userIDTracking'] && $this->user_id){
+			$config['user_id'] = $this->user_id;
 		}
 
-		if ($pageviewTracking){
+		if ($this->config['pageviewTracking']){
 			$config['send_page_view'] = false;
 		}
 
-		if ($siteCurrency){
-			$config['currency'] = $siteCurrency;
+		if ($this->config['siteCurrency']){
+			$config['currency'] = $this->config['siteCurrency'];
 		}
 
-		if ($userPerformanceTiming){
+		if ($this->config['userPerformanceTiming']){
 			$buffer .= $this->userPerformanceTiming();
 		}
 
-		if ($enhancedLinkAttribution){
+		if ($this->config['enhancedLinkAttribution']){
 			$config['link_attribution'] = true;
 		}
 
-		if ($ipAnonymize){
+		if ($this->config['ipAnonymize']){
 			$config['anonymize_ip'] = true;
 		}
 
-		if ($forceSSL){
+		if ($this->config['forceSSL']){
 			$config['forceSSL'] = true;
 		}
 
-		$buffer .= '		gtag(\'config\', \''.$trackingID.'\', '.json_encode($config).');
+		$buffer .= '	gtag(\'config\', \'' . $this->config['trackingID'] . '\', ' . json_encode($config) . ');
 ';
 
-		if ($trackingID2){
-			$buffer .= '		gtag(\'config\', \''.$trackingID2.'\', '.json_encode($config).');
+		if ($this->config['trackingID2']){
+			$buffer .= '	gtag(\'config\', \'' . $this->config['trackingID2'] . '\', ' . json_encode($config) . ');
 ';
 		}
 
-		$buffer .= '		</script>';
+		$buffer .= '	</script>
+';
 
 		return $buffer;
 	}
 
 	private function googleTagManager() {
-		$containerID = $this->params->get('containerID');
-
 		return '
-		<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
-		new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
-		j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
-		\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
-		})(window,document,\'script\',\'dataLayer\',\''.$containerID.'\');</script>';
+	<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({\'gtm.start\':
+	new Date().getTime(),event:\'gtm.js\'});var f=d.getElementsByTagName(s)[0],
+	j=d.createElement(s),dl=l!=\'dataLayer\'?\'&l=\'+l:\'\';j.async=true;j.src=
+	\'https://www.googletagmanager.com/gtm.js?id=\'+i+dl;f.parentNode.insertBefore(j,f);
+	})(window,document,\'script\',\'dataLayer\',\'' . $this->config['containerID'] . '\');</script>';
 	}
 }
 ?>
